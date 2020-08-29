75c79f9d00cba2cd27078d560af72180cf2ac737
Danuel <public.danuel@gmail.com>
2020-08-29 22:00:21 +0900

    add crates: {cli, macro, runtime: {ir, jvm, wasm}, std, vm}

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-fn main () {
+pub use danube_compiler::*;
+pub use danube_ir::*;
+pub use danube_parser::*;
+
+fn main() {
   println!("Danube is still in development.");
   println!("If you are interested in developing a programming language, please contact us!");
 }
